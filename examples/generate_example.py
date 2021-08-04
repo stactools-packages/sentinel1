00f42937ac2b6fbcb2b19e01fa81c80ea24eb214
Scott Henderson <scottyh@uw.edu>
2021-08-04 15:43:54 -0700

    update example with valid asset hrefs

--- examples/generate_example.py
+++ examples/generate_example.py
@@ -12,11 +12,12 @@ catalog = pystac.Catalog(id='sentinel1-rtc-example',
 
 collection = stac.create_collection()
 
-item1 = stac.create_item('../tests/data-files/S1A_20200103_17RMJ_ASC',
-                         asset='local_incident_angle.vrt')
+base_href = 'https://sentinel-s1-rtc-indigo.s3.us-west-2.amazonaws.com'
+key = 'tiles/RTC/1/IW/17/R/MJ/2020/S1A_20200103_17RMJ_ASC'
+item1 = stac.create_item(f'{base_href}/{key}')
 
-item2 = stac.create_item('../tests/data-files/S1B_20161121_12SYJ_ASC',
-                         asset='local_incident_angle.vrt')
+key = 'tiles/RTC/1/IW/12/S/YJ/2016/S1B_20161121_12SYJ_ASC'
+item2 = stac.create_item(f'{base_href}/{key}')
 
 collection.add_items([item1, item2])
 
@@ -26,6 +27,6 @@ catalog.generate_subcatalogs(template='${year}')
 catalog.normalize_hrefs('./')
 # NOTE: can simplify after https://github.com/stac-utils/pystac/pull/565/files
 #published_root_url = 'https://raw.githubusercontent.com/stactools-packages/sentinel1/main/examples/catalog.json'
-#catalog.set_self_href(published_root_url) #manually change after saving
+#catalog.set_self_href(published_root_url) # !!! manually change after saving !!!
 catalog.validate_all()
 catalog.save()

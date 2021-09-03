92d3b88d382c6b82cbb6408226136bf560b1c1ee
Rob Emanuele <rdemanuele@gmail.com>
2021-09-02 22:36:49 -0400

    Change import style for top-level command registration

--- src/stactools/sentinel1/commands.py
+++ src/stactools/sentinel1/commands.py
@@ -1,7 +1,7 @@
 import click
 
-from stactools.sentinel1.grd import commands as grd_commands
-from stactools.sentinel1.rtc import commands as rtc_commands
+from stactools.sentinel1.grd.commands import grd_cmd
+from stactools.sentinel1.rtc.commands import rtc_cmd
 
 
 def create_sentinel1_command(cli: click.Group) -> click.Command:
@@ -10,7 +10,7 @@ def create_sentinel1_command(cli: click.Group) -> click.Command:
         """Commands for working with sentinel1 data"""
         pass
 
-    sentinel1_cmd.add_command(grd_commands.grd_cmd)
-    sentinel1_cmd.add_command(rtc_commands.rtc_cmd)
+    sentinel1_cmd.add_command(grd_cmd)
+    sentinel1_cmd.add_command(rtc_cmd)
 
     return sentinel1_cmd

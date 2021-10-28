1be05b672402421ec205d56f24756a1186caf601
Jan Makovecki <jan.makovecki@gmail.com>
2021-10-28 20:24:16 +0200

    Improve shapely geometry loading

--- src/stactools/sentinel1/grd/product_metadata.py
+++ src/stactools/sentinel1/grd/product_metadata.py
@@ -1,12 +1,10 @@
-import json
 from datetime import datetime
 from typing import Any, Callable, Dict, List, Optional
 
 from pystac.utils import str_to_datetime
-from shapely.geometry import Polygon, mapping  # type: ignore
+from shapely.geometry import Polygon, mapping, MultiPolygon, LineString  # type: ignore
 from stactools.core.io import ReadHrefModifier
 from stactools.core.io.xml import XmlElement
-
 from stactools.sentinel1.grd.metadata_links import MetadataLinks
 
 
@@ -58,35 +56,21 @@ class ProductMetadata:
                 geometry = mapping(footprint_polygon)
                 bbox = footprint_polygon.bounds
 
-                return bbox, geometry
-
             else:
-                geometry = product_info["footprint"]["coordinates"]
+                coordinates = product_info["footprint"]["coordinates"]
+                geom_type = str(product_info["footprint"]["type"])
 
-                def parse_polygon(geom: List):
-                    def fail():
-                        raise RuntimeError(
-                            f"Can't parse Polygon from: {json.dumps(geom)} for {href}"
-                        )
+                if geom_type.lower() == "polygon":
+                    footprint_geom = Polygon(*coordinates)
+                elif geom_type.lower() == "multipolygon":
+                    footprint_geom = MultiPolygon(coordinates, context_type="geojson")
+                else:
+                    raise RuntimeError("Can't read geometry of type " + geom_type)
 
-                    if len(geom) > 1:
-                        if not isinstance(geom[0], List):
-                            fail()
-                        if len(geom[0]) == 2:
-                            if not isinstance(geom[0][0], float):
-                                fail()
-                            return Polygon([(point[0], point[1])
-                                            for point in geom])
-                    elif len(geom) == 1:
-                        return parse_polygon(geom[0])
-                    else:
-                        fail()
+                geometry = mapping(footprint_geom)
+                bbox = footprint_geom.bounds
 
-                footprint_polygon = parse_polygon(geometry)
-                geometry = mapping(footprint_polygon)
-                bbox = footprint_polygon.bounds
-
-                return bbox, geometry
+            return bbox, geometry
 
         self.bbox, self.geometry = _get_geometries()
 

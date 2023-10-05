bbde8202b1b18e6ef82dadeb370baa9641ef09ea
Fabian Schindler <fabian.schindler.strauss@gmail.com>
2023-10-05 09:30:58 +0200

    Adding tests for SLC Adding test SAFE package Fixing GRD tests

---
+++ tests/slc/test_commands.py
@@ -0,0 +1,74 @@
+import os
+from pathlib import Path
+from tempfile import TemporaryDirectory
+from typing import Callable, List
+
+import pystac
+from click import Command, Group
+from pystac.extensions.eo import EOExtension
+from pystac.utils import is_absolute_href
+from stactools.testing.cli_test import CliTestCase
+
+from stactools.sentinel1.commands import create_sentinel1_command
+from stactools.sentinel1.slc.constants import SENTINEL_POLARIZATIONS
+from tests import test_data
+
+
+class CreateItemTest(CliTestCase):
+    def create_subcommand_functions(self) -> List[Callable[[Group], Command]]:
+        return [create_sentinel1_command]
+
+    def test_create_collection(self) -> None:
+        with TemporaryDirectory() as tmp_dir:
+            tmp_path = Path(tmp_dir)
+            destination = tmp_path / "sentinel1-slc.json"
+            cmd = ["sentinel1", "slc", "create-collection", f"{tmp_path}"]
+            result = self.run_command(cmd)
+
+            assert result.exit_code == 0, "\n{}".format(result.output)
+            paths = [p for p in tmp_path.iterdir() if p.suffix == ".json"]
+
+            assert len(paths) == 1
+            collection = pystac.Collection.from_file(str(destination))
+
+            assert collection.id == "sentinel1-slc"
+            # collection.set_self_href(
+            #    str(destination)
+            # )  # Must set the self reference to pass validation
+            collection.validate()
+
+    def test_create_item(self) -> None:
+        item_id = "S1A_IW_SL1__1_SH_20141031T095929_20141031T100002_003072_003842"
+        granule_href = test_data.get_path(
+            "data-files/slc/S1A_IW_SL1__1_SH_20141031T095929_20141031T100002_003072_003842_91FC.SAFE"  # noqa
+        )
+
+        with self.subTest(granule_href):
+            with TemporaryDirectory() as tmp_dir:
+                cmd = ["sentinel1", "slc", "create-item", granule_href, tmp_dir]
+                self.run_command(cmd)
+
+                jsons = [p for p in os.listdir(tmp_dir) if p.endswith(".json")]
+                self.assertEqual(len(jsons), 1)
+                fname = jsons[0]
+
+                item = pystac.Item.from_file(os.path.join(tmp_dir, fname))
+
+                item.validate()
+
+                self.assertEqual(item.id, item_id)
+
+                bands_seen = set()
+
+                for _, asset in item.assets.items():
+                    # Ensure that there's no relative path parts
+                    # in the asset HREFs
+                    self.assertTrue("/./" not in asset.href)
+                    self.assertTrue(is_absolute_href(asset.href))
+                    asset_eo = EOExtension.ext(asset)
+                    bands = asset_eo.bands
+                    if bands is not None:
+                        bands_seen |= set(b.name for b in bands)
+
+                for x in bands_seen:
+                    self.assertTrue(x.lower() in list(SENTINEL_POLARIZATIONS.keys()))
